266698c4bd81dd32d151fbe9b7c59e72e1c35d61
Martin J. Klöckner <martin.cachari@gmail.com>
2020-11-13 21:03:07 -0300

    Added simple degrees program, corresponding to exercise "ej7.c" from the c-repo

---
+++ degree.py
@@ -0,0 +1,11 @@
+degree = int(input("Ingrese un angulo: "))
+
+if degree < 90:
+	print("El angulo ingresado es AGUDO")
+
+elif degree == 90:
+	print("El angulo ingresado es RECTO")
+
+elif degree > 90:
+	print("El angulo ingresado es OBTUSO")
+
